b4dd991b88bedfd1568f36c9f69cb34afae15515
AnishSandaye <101982127+AnishSandaye@users.noreply.github.com>
2025-07-22 14:31:03 +0530

    Add files via upload

---
+++ ROS 2/SLAM/Build/turtlebot3/colcon_command_prefix_build.sh
@@ -0,0 +1,8 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/anish/turtlebot3_ws/install/dynamixel_sdk/share/dynamixel_sdk/package.sh"
+. "/home/anish/turtlebot3_ws/install/turtlebot3_description/share/turtlebot3_description/package.sh"
+. "/home/anish/turtlebot3_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
+. "/home/anish/turtlebot3_ws/install/turtlebot3_navigation2/share/turtlebot3_navigation2/package.sh"
+. "/home/anish/turtlebot3_ws/install/turtlebot3_teleop/share/turtlebot3_teleop/package.sh"
+. "/home/anish/turtlebot3_ws/install/turtlebot3_example/share/turtlebot3_example/package.sh"
+. "/home/anish/turtlebot3_ws/install/turtlebot3_node/share/turtlebot3_node/package.sh"
